82072f7485e1a4b92933e229c89be5d2163343f3
scristo2 <77905570+scristo2@users.noreply.github.com>
2023-04-30 23:14:03 +0200

    Update bearerrEQUIEREDhttp.php

--- bearerrEQUIEREDhttp.php
+++ bearerrEQUIEREDhttp.php
@@ -8,3 +8,4 @@ $bearer =  $_SERVER['HTTP_AUTHORIZATION'];
 luego compararramos con un if si bearers son igual desde el fecth que se ha enviado,este ejemplo es desde next js
 
 const res = await axios.get(`${process.env.URL_GETREVIEWS}`, {headers : {Authorization : `Bearer ${process.env.BEARER_NAMEQUEQUIERAS}`}})
+ 
